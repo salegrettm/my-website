370d9162322a1bca402cbb1db62e0e0c517ba797
Sebastian Alegrett Morreo <salegrett-morreo@huskers.unl.edu>
2022-03-30 07:37:08 -0500

    Fixed about me and portfolio section styling

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -33,56 +33,52 @@ function Portfolio() {
 
   return (
     <div className='portfolio-wrapper'>
-      <div className='projects-wrapper'>
-        {
-          projects.map((project) => {
-            return (
-              <div className='project' key={project.title}>
-                <h2 className='project-title'>{project.title}</h2>
-                <div className='project-images'>
-                  {
-                    project.images.map((img, idx) => {
-                      return <img src={img} key={idx} alt='project' className='project-image' />
-                    })
-                  }
-                </div>
-                <label className='project-subheader'>
-                  <strong>Company: </strong>{project.company}
-                </label>
-                <label className='project-subheader'>
-                  <strong>Technologies: </strong>{project.technologies}
-                </label>
-                <label className='project-subheader'>
-                  <strong>Description:</strong>
-                </label>
-                <p className='project-description'>
-                  {project.description}
-                </p>
-                <div className='project-links'>
-                  <label className='project-subheader'>Link:</label>
-                  <a className='project-link' rel="noreferrer" target='_blank' href={project.link}><FaIcons.FaLink /></a>
-                  <label className='project-subheader'>Github:</label>
-                  <a className='project-link' rel="noreferrer" target='_blank' href={project.link}><FaIcons.FaGithub /></a>
-                </div>
+      {
+        projects.map((project) => {
+          return (
+            <div className='project' key={project.title}>
+              <h2 className='project-title'>{project.title}</h2>
+              <div className='project-images'>
+                {
+                  project.images.map((img, idx) => {
+                    return <img src={img} key={idx} alt='project' className='project-image' />
+                  })
+                }
               </div>
-            );
-          })
-        }
-      </div>
-      <div className='upcoming-section'>
-        <h2 className='project-title'>Info and Upcoming</h2>
-        <p className='paragraph'>
-          This website was entirely developed in React.js. In here I showcase my front-end knowledge and design.
-          Currently, I am working on finishing my degree and freeCodeCamp certifications.
-          In a few months this will make me eligible to develop a charity project for a non-profit organization of my
-          choice. Be on the look out for that!
-        </p>
-        <p className='paragraph'>
-          Additionally, below you will find my completed freeCodeCamp certifications.
-          I have linked to my freeCodeCamp profile, were you can see the curriculum and projects
-          I have completed to earn them. Feel free to check them out on my CodePen linked below.
-        </p>
-      </div>
+              <label className='project-subheader'>
+                <strong>Company: </strong><br/>{project.company}
+              </label>
+              <label className='project-subheader'>
+                <strong>Technologies: </strong><br/>{project.technologies}
+              </label>
+              <label className='project-subheader'>
+                <strong>Description:</strong>
+              </label>
+              <p className='project-description'>
+                {project.description}
+              </p>
+              <div className='project-links'>
+                <label className='project-subheader'>Link:</label>
+                <a className='project-link' rel="noreferrer" target='_blank' href={project.link}><FaIcons.FaLink /></a>
+                <label className='project-subheader'>Github:</label>
+                <a className='project-link' rel="noreferrer" target='_blank' href={project.link}><FaIcons.FaGithub /></a>
+              </div>
+            </div>
+          );
+        })
+      }
+      <h2 className='project-title'>Info and Upcoming</h2>
+      <p className='paragraph'>
+        This website was entirely developed in React.js. In here I showcase my front-end knowledge and design.
+        Currently, I am working on finishing my degree and freeCodeCamp certifications.
+        In a few months this will make me eligible to develop a charity project for a non-profit organization of my
+        choice. Be on the look out for that!
+      </p>
+      <p className='paragraph'>
+        Additionally, below you will find my completed freeCodeCamp certifications.
+        I have linked to my freeCodeCamp profile, were you can see the curriculum and projects
+        I have completed to earn them. Feel free to check them out on my CodePen linked below.
+      </p>
     </div>
   )
 }
