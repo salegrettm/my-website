33f04f5d46d42138de6f5aa5824e7d31200eed13
Sebastian Alegrett Morreo <salegrett-morreo@huskers.unl.edu>
2022-03-31 14:01:24 -0500

    Certifications slight updates

--- src/components/Certifications.js
+++ src/components/Certifications.js
@@ -32,57 +32,45 @@ function Certifications() {
   return (
     <div className='certifications-wrapper'>
       <h1 className='certifications-title'>Completed</h1>
-      <div className='completed-section'>
-        {
-          certs.map((cert) => {
-            return (
-              <div className='certification' key={cert.name}>
-                <h2 className='cert-title'>{cert.name}</h2>
-                {cert.icon}
-                <label className='cert-label'>
-                  <strong>Issuing Organization: </strong>
-                  {' ' + cert.organization}
-                </label>
-                <label className='cert-label'>
-                  <strong>Time to Complete: </strong>
-                  ~{cert.time}
-                </label>
-                <label className='cert-label'>
-                  <strong>Link: </strong>
-                  <a
-                    className='cert-link'
-                    rel="noreferrer"
-                    target='_blank'
-                    href={cert.link}
-                  >
-                    <FaIcons.FaLink />
-                  </a>
-                </label>
-              </div>
-            );
-          })
-        }
-      </div>
+      {
+        certs.map((cert) => {
+          return (
+            <a
+              className='certification'
+              rel="noreferrer"
+              target='_blank'
+              href={cert.link}
+              key={cert.name}
+            >
+              <h2 className='cert-title'>{cert.name}</h2>
+              {cert.icon}
+              <label className='cert-label'>
+                <strong>Issuing Organization: </strong>
+                {' ' + cert.organization}
+              </label>
+              <label className='cert-label'>
+                <strong>Time to Complete: </strong>
+                ~{cert.time}
+              </label>
+            </a>
+          );
+        })
+      }
       <h1 className='certifications-title'>Upcoming</h1>
       <div className='cert-upcoming-section'>
         <label className='cert-label'>
-          <strong>Certification: </strong>
           Front End Developer (1200 hrs) - April 2022
         </label>
         <label className='cert-label'>
-          <strong>Certification: </strong>
           Data Visualization (300 hrs) - May 2022
         </label>
         <label className='cert-label'>
-          <strong>Certification: </strong>
           Back End Developer (1200 hrs) - August 2022
         </label>
         <label className='cert-label'>
-          <strong>Certification: </strong>
           Full Stack Developer (2400 hrs) - August 2022
         </label>
         <label className='cert-label'>
-          <strong>Certification: </strong>
           Bachelor of Science in Computer Science - December 2022
         </label>
       </div>
@@ -90,12 +78,12 @@ function Certifications() {
       <div className='cert-info'>
         <p className='cert-paragraph'>
           All of these certifications are completed through freeCodeCamp with the exception of my degree.
-          Click <a className='cert-link' rel="noreferrer" target='_blank' href='https://www.freecodecamp.org/salegrettm'>here</a> 
+          Click <a className='cert-link' rel="noreferrer" target='_blank' href='https://www.freecodecamp.org/salegrettm'>here</a>
           &nbsp; to view my freeCodeCamp profile and look at the curriculum I had to go through to obtain them.
         </p>
         <p className='cert-paragraph'>
           Additionally, go checkout my codePen to see the small projects I worked on to complete the certifications.
-          Click <a className='cert-link' rel="noreferrer" target='_blank' href='https://www.freecodecamp.org/salegrettm'>here</a> 
+          Click <a className='cert-link' rel="noreferrer" target='_blank' href='https://www.freecodecamp.org/salegrettm'>here</a>
           &nbsp; to view my codePen profile and look at the projects I had to complete.
         </p>
       </div>
